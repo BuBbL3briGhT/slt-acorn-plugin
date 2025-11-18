2187dd964cf8202ab9ff424ed1a83b8425a649e1
BaMBi <bAmBi@baCKdOor.niL>
2025-11-18 07:55:52 -0500

    . Sparse test to ensure sltPlugin (a factory function which produces an SLTParser upon a variable BaseClass) is doing what it's expected of.

--- test/slt_plugin.js
+++ test/slt_plugin.js
@@ -2,3 +2,12 @@ import { describe, it } from "node:test";
 import assert from "node:assert";
 
 import { sltPlugin } from "../src/slt_plugin.js";
+
+describe("sltPlugin", function () {
+  it("returns an SLTParser instance", function () {
+    assert(sltPlugin(function(){}));
+  });
+});
+
+
+
