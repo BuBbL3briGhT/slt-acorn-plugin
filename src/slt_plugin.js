676aeafe0ec087611f313c76df3ee34248435a9f
BaMBi <bAmBi@baCKdOor.niL>
2025-11-24 10:02:58 -0500

    . Format src/slt_parser.js for 66 chars.

--- src/slt_plugin.js
+++ src/slt_plugin.js
@@ -32,38 +32,56 @@ export function sltPlugin(BaseParser) {
       const keyword = String(this.value);
 
       // consume keyword
-      if (typeof this.next === "function") this.next();
+      if (typeof this.next === "function")
+        this.next();
       this._isSLTToken = false;
 
       // simple argument form: slt_print "hello";
       if (this.type !== acorn.tokTypes.braceL) {
         let value = "";
-        while (!this.eof() && this.type !== acorn.tokTypes.braceL && this.type !== acorn.tokTypes.semi && !this._isSLTToken) {
+        while (!this.eof() &&
+            this.type !== acorn.tokTypes.braceL
+            && this.type !== acorn.tokTypes.semi
+            && !this._isSLTToken) {
           value += this.value + " ";
-          if (typeof this.next === "function") this.next();
+          if (typeof this.next === "function")
+            this.next();
           else break;
         }
-        if (this.type === acorn.tokTypes.semi && typeof this.next === "function") this.next();
-        return { type: "SLTExpression", keyword, value: value.trim(), start, end: this.lastTokEnd };
+        if (this.type === acorn.tokTypes.semi
+            && typeof this.next === "function")
+          this.next();
+        return {
+          type: "SLTExpression", keyword,
+          value: value.trim(), start,
+          end: this.lastTokEnd
+        };
       }
 
       // block form: slt_block { ... }
       if (this.type === acorn.tokTypes.braceL) {
-        if (typeof this.next === "function") this.next(); // consume '{'
+        if (typeof this.next === "function")
+          this.next(); // consume '{'
         const body = [];
         while (this.type !== acorn.tokTypes.braceR) {
-          if (this._isSLTToken && typeof this.parseSLT === "function") {
+          if (this._isSLTToken &&
+              typeof this.parseSLT === "function") {
             body.push(this.parseSLT());
           } else {
-            if (typeof this.next === "function") this.next();
+            if (typeof this.next === "function")
+              this.next();
             else break;
           }
         }
-        if (this.type === acorn.tokTypes.braceR && typeof this.next === "function") this.next();
-        return { type: "SLTExpression", keyword, body, start, end: this.lastTokEnd };
+        if (this.type === acorn.tokTypes.braceR &&
+            typeof this.next === "function")
+          this.next();
+        return { type: "SLTExpression", keyword,
+          body, start, end: this.lastTokEnd };
       }
 
-      this.raise && this.raise(start, `Unexpected SLT keyword: ${keyword}`);
+      this.raise && this.raise(start,
+        `Unexpected SLT keyword: ${keyword}`);
     }
   };
 }
