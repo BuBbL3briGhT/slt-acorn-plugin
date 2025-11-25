1994e83ff2a6514a008a1c087c3a9ed9da2cb2c2
BaMBi <bAmBi@baCKdOor.niL>
2025-11-24 19:39:10 -0500

    . Run some code through the parser. Just following a quick example from the acorn Readme.

--- test/slt_plugin.js
+++ test/slt_plugin.js
@@ -3,6 +3,21 @@ import assert from "node:assert";
 
 import { sltPlugin } from "../src/slt_plugin.js";
 
+// const { Parser } = require("acorn");
+import { Parser } from "acorn";
+
+const SLTParser = sltPlugin(Parser);
+
+console.log({kool: SLTParser.parse(
+   "// Some bigint + JSX code")})
+
+console.log({kool: SLTParser.parse(
+   "print('hello');")})
+
+console.log({kool: SLTParser.parse(
+   "slt_print('hello');")})
+
+
 describe("sltPlugin", function () {
   it("returns an SLTParser instance", function () {
     assert(sltPlugin(function(){}));
