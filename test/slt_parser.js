144e2259e5a940bbf6f4066bd7fc030b601d2be1
BaMBi <bAmBi@baCKdOor.niL>
2025-11-25 06:36:36 -0500

    . test/slt_parser.js

---
+++ test/slt_parser.js
@@ -0,0 +1,25 @@
+import { describe, it, before } from "node:test";
+import assert from "node:assert";
+
+import { Parser } from "acorn";
+
+import { sltPlugin } from "../src/slt_plugin.js";
+
+let SLTParser;
+
+describe("SLTParser", function() {
+
+  before(function () {
+    SLTParser = sltPlugin(Parser);
+  });
+
+  describe("parse", function () {
+    it("parses an es return statement", function () {
+      // const resultado = SLTParser.parse(`función () { vuelta 42; }`)
+      const resultado = SLTParser.parse(`function () { vuelta 42; }`)
+      console.log(resultado.body);
+    });
+  });
+
+});
+
