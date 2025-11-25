98b8844f44a19fe768f21bc8b9d4114d02000cfc
BaMBi <bAmBi@baCKdOor.niL>
2025-11-25 10:40:01 -0500

    . A little bit hacky, but runs clean through and parses si.
        acorn-plugin 🍀 node test/parser.js
        hooking parse.
        /^(?:si)$/
        !!!hooking parseStatement TokenType {
          label: 'si',
          keyword: 'si',
          beforeExpr: false,
          startsExpr: false,
          isLoop: false,
          isAssign: false,
          prefix: false,
          postfix: false,
          binop: null,
          updateContext: null
        }
        Parse MyStatement
        parse MyStatement
        Type:  TokenType {
          label: 'name',
          keyword: undefined,
          beforeExpr: false,
          startsExpr: true,
          isLoop: false,
          isAssign: false,
          prefix: false,
          postfix: false,
          binop: null,
          updateContext: [Function (anonymous)]
        }
        !!!hooking parseStatement TokenType {
          label: '{',
          keyword: undefined,
          beforeExpr: true,
          startsExpr: true,
          isLoop: false,
          isAssign: false,
          prefix: false,
          postfix: false,
          binop: null,
          updateContext: [Function (anonymous)]
        }
        !!!hooking parseStatement TokenType {
          label: 'num',
          keyword: undefined,
          beforeExpr: false,
          startsExpr: true,
          isLoop: false,
          isAssign: false,
          prefix: false,
          postfix: false,
          binop: null,
          updateContext: null
        }
        {
         "type": "Program",
         "start": 0,
         "end": 27,
         "body": [
          {
           "type": "IfStatement",
           "start": 3,
           "end": 26,
           "test": {
            "type": "Identifier",
            "start": 7,
            "end": 11,
            "name": "true"
           },
           "isSi": true,
           "consequent": {
            "type": "BlockStatement",
            "start": 13,
            "end": 26,
            "body": [
             {
              "type": "ExpressionStatement",
              "start": 19,
              "end": 22,
              "expression": {
               "type": "Literal",
               "start": 19,
               "end": 21,
               "value": 42,
               "raw": "42"
              }
             }
            ]
           }
          }
         ],
         "sourceType": "script"
        }

--- test/parser.js
+++ test/parser.js
@@ -1,14 +1,112 @@
 
-import { Parser } from "acorn";
+import { Parser, tokTypes, TokenType } from "acorn";
 
 var program =
 `
-  if (true) {
+  si (true) {
     42;
   }
 `;
 
-var result = Parser.parse(program);
+const tt = tokTypes;
+
+Parser.acorn.keywordTypes["si"] =
+  new TokenType("si",
+    { keyword: "si" });
+
+function wordsRegexp(words) {
+  return new RegExp("^(?:" +
+    words.replace(/ /g, "|") + ")$")
+}
+
+function makeParser(BaseParser) {
+  return class extends BaseParser {
+
+    parse(program) {
+      console.log("hooking parse.");
+
+      const newKeywords = "si";
+      console.log(wordsRegexp(newKeywords));
+
+      this.keywords = wordsRegexp(newKeywords);
+
+      return(super.parse(program));
+    }
+
+
+    parseStatement(context, topLevel, exports) {
+      var starttype = this.type;
+      console.log("!!!hooking parseStatement",
+        starttype);
+
+      if (starttype ==
+          Parser.acorn.keywordTypes["si"]) {
+        console.log("Parse MyStatement");
+        var node = this.startNode();
+        return this.parseSi(node);
+      }
+      else {
+        return(super.parseStatement(context,
+          topLevel, exports));
+      }
+    }
+
+    parseSi(node) {
+      console.log("parse MyStatement");
+      this.next();
+
+      //In my language, MyStatement doesn't have to have a parameter. It could be called as `MyStatement { ... }`
+      if (this.type == tt.parenL) {
+        node.test =
+          this.parseOptionalParenExpression();
+      }
+      else {
+        node.test = 0;
+        //If there is no test, just make it 0 for now
+        //(note that this may break code generation
+        //later).
+      }
+
+      node.isSi = true;
+      //set a flag so we know that this if a
+      //"MyStatement" instead of an if statement.
+
+      //process the body of the block just like a
+      //normal if statement for now.
+
+      // allow function declarations in branches, but
+      // only in non-strict mode
+      node.consequent = this.parseStatement("if");
+      //node.alternate = this.eat(acornTypes["else"])
+      //? this.parseStatement("if") : null;
+      return this.finishNode(node, "IfStatement")
+    };
+
+    parseOptionalParenExpression() {
+      this.expect(tt.parenL);
+
+      //see what type it is
+      console.log("Type: ", this.type);
+
+      //allow it to be blank.
+      var val = 0;
+      //for now just make the condition 0. Note that
+      //this may break code generation later.
+      if (this.type == tt.parenR) {
+        this.expect(tt.parenR);
+      }
+      else {
+        val = this.parseExpression();
+        this.expect(tt.parenR);
+      }
+
+      return val
+    };
+
+  }
+}
+
+var result = makeParser(Parser).parse(program);
 
 console.log(JSON.stringify(result ,null,' '));
 
