94aaa8964b95590185a6d460564233b4054cee77
BaMBi <bAmBi@baCKdOor.niL>
2025-11-27 13:28:43 -0500

    🌞 https://youtu.be/IHTaMMyK274?
      acorn-plugin 🍀 node test/parser2.js
    
      {
       "type": "Program",
       "start": 0,
       "end": 53,
       "body": [
        {
         "type": "IfStatement",
         "start": 3,
         "end": 26,
         "test": {
          "type": "Identifier",
          "start": 7,
          "end": 11,
          "name": "true"
         },
         "consequent": {
          "type": "BlockStatement",
          "start": 13,
          "end": 26,
          "body": [
           {
            "type": "ExpressionStatement",
            "start": 19,
            "end": 22,
            "expression": {
             "type": "Literal",
             "start": 19,
             "end": 21,
             "value": 42,
             "raw": "42"
            }
           }
          ]
         },
         "alternate": null
        },
        {
         "type": "FunctionDeclaration",
         "start": 30,
         "end": 52,
         "id": {
          "type": "Identifier",
          "start": 38,
          "end": 47,
          "name": "miFunción"
         },
         "expression": false,
         "generator": false,
         "async": false,
         "params": [],
         "body": {
          "type": "BlockStatement",
          "start": 50,
          "end": 52,
          "body": []
         }
        }
       ],
       "sourceType": "script"
      }

--- test/parser2.js
+++ test/parser2.js
@@ -17,11 +17,11 @@ function wordsRegexp(words) {
     words.replace(/ /g, "|") + ")$")
 }
 
-const _si = Parser.acorn.keywordTypes["si"] =
-  new TokenType("si", { keyword: "si" });
+tt._if.keyword = "si";
+tt._function.keyword = "función";
 
-const _función = Parser.acorn.keywordTypes["función"] =
-  new TokenType("función", { keyword: "función" });
+Parser.acorn.keywordTypes["si"] = tt._if;
+Parser.acorn.keywordTypes["función"] = tt._function;
 
 function makeParser(BaseParser) {
   return class extends BaseParser {
@@ -31,23 +31,6 @@ function makeParser(BaseParser) {
       const newKeywords = "si función";
       this.keywords = wordsRegexp(newKeywords);
       return this;
-    }
-
-    parseStatement(context, topLevel, exports) {
-      let starttype = this.type, node = this.startNode(), kind
-
-      switch(starttype) {
-        case _función:
-          // Function as sole body of either an if statement or a labeled statement
-          // works, but not when it is part of a labeled statement that is the sole
-          // body of an if statement.
-          if ((context && (this.strict || context !== "if" && context !== "label")) && this.options.ecmaVersion >= 6) this.unexpected()
-          return this.parseFunctionStatement(node, false, !context)
-        case _si: return this.parseIfStatement(node);
-      }
-
-      return(super.parseStatement(context, topLevel,
-        exports));
     }
 
   }
